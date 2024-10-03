42378122bfac0e58c92453a852b4b6ee11d5924d
Sanket Deshmukh <137975053+SanketDeshmukh007@users.noreply.github.com>
2024-10-03 09:33:08 +0530

    Add files via upload

---
+++ Asynchronous/3 Callback Hell.js
@@ -0,0 +1,50 @@
+
+function task1(callback)
+{
+    setTimeout(() => {
+        console.log('Task 1 Complete')
+        callback()
+    }, 2000);
+}
+
+function task2(callback)
+{
+    setTimeout(() => {
+        console.log('Task 2 Complete')
+        callback()
+    }, 2000);
+}
+
+function task3(callback)
+{
+    setTimeout(() => {
+        console.log('Task 3 Complete')
+        callback()
+    }, 2000);
+}
+
+function task4(callback)
+{
+    setTimeout(() => {
+        console.log('Task 4 Complete')
+        callback()
+    }, 2000);
+}
+
+function task5()
+{
+    setTimeout(() => {
+        console.log('Task 5 Complete')       
+    }, 2000);
+}
+
+task1(()=>{
+    task2(()=>{
+        task3(()=>{
+            task4(()=>{
+                task5()
+            })
+        })
+    })
+})
+
